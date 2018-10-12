59ddfd75d4b9f2d97acb15d1a59006a3f9d0d62b
Donald Cross <dcross@iguanahost.com>
2018-10-12 17:21:29 -0400

    test dev branch

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -12,7 +12,7 @@ export default class MainScreen extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Hello Test Dev branch 2</Text>
+        <Text>Hello Test Dev branch 3</Text>
       </View>
     );
   }
